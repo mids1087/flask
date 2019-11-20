85207715969cc1c0c9e52175ac32df108c1c847d
familyshengbill@gmail.com <familyshengbill@gmail.com>
2019-11-20 00:08:55 -0600

    try git commit -a if it works without add

--- Project_One/hello/hello.py
+++ Project_One/hello/hello.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 def index():
     return '<h1>Greet, %s!</h1>' % __name__
 
-# test commit from .py
+# test commit from .py 必须要在.git 主目录下commit 不然就在commit 后面添加 -a 所有
+# try if commit -a works without add
